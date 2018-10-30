b51136675973af910e833aa583e59050ca1a13d3
ninjerie <38001901+ninjerie@users.noreply.github.com>
2018-10-30 17:16:38 +0000

    Updates
    Seperated work completed at home and work completed in uni.
    Added work completed in lesson.

---
+++ Prototype/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,30 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+*/
+
+
+using UnityEngine;
+// Lets gameobjects talk to each other
+using UnityEngine.Events;
+[System.Serializable]
+public class OnDamagedEvent : UnityEvent<int> { }
+
+public class HealthSystem : MonoBehaviour {
+
+    public int health = 10;
+    // A unity event
+    public UnityEvent onDie;
+
+    public OnDamagedEvent onDamaged;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        onDamaged.Invoke(health);
+        if (health < 1)
+        {
+            onDie.Invoke();
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/Weapon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float fireTime = 0.5f;    public bool isFiring = false;
+
+    public void SetFiring()
+    {
+        isFiring = false;
+    }
+
+    public void Fire()
+    {
+        isFiring = true;
+        Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        Invoke("SetFiring", fireTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isFiring)
+            {
+                Fire();
+            }
+        }
+    }
+
+}
