1f7c6c1b9eb514dfd50247e5d43b84e2e4829da9
ninjerie <38001901+ninjerie@users.noreply.github.com>
2018-11-27 17:01:16 +0000

    Updates
    Made the score appear on the end screen.

--- My Game/Sauce Wars/Assets/Scripts/GameUI.cs
+++ My Game/Sauce Wars/Assets/Scripts/GameUI.cs
@@ -8,8 +8,11 @@ public class GameUI : MonoBehaviour {
     public int playerScore = 0;
     public int enemyScore = 0;
 
-    public Text playerSceneScoreText;
-    public Text enemySceneScoreText;
+    //public Text playerSceneScoreText;
+    //public Text enemySceneScoreText;
+
+    public delegate void UpdateScore(int theScore);
+    public static event UpdateScore UpdateCharacterScore;
 
     public Text PlayerScoreText;
     public Text EnemyScoreText;
@@ -30,6 +33,17 @@ public class GameUI : MonoBehaviour {
         //AddScore.OnSendScore -= UpdateScore;
         AddScore.OnSendScorePlayer -= UpdateScorePlayer;
         AddScore.OnSendScoreEnemy -= UpdateScoreEnemy;
+
+        int currentPlayerScore = PlayerPrefs.GetInt("PlayerScore");
+        if (currentPlayerScore != playerScore)
+        {
+            PlayerPrefs.SetInt("PlayerScore", playerScore);
+        }
+        int currentEnemyScore = PlayerPrefs.GetInt("EnemyScore");
+        if (currentEnemyScore != enemyScore)
+        {
+            PlayerPrefs.SetInt("EnemyScore", enemyScore);
+        }
     }
 
     private void UpdateScorePlayer(int Score)
@@ -39,7 +53,10 @@ public class GameUI : MonoBehaviour {
         //print("updating score");
         playerScore += Score;
         PlayerScoreText.text = "SCORE: " + playerScore.ToString();
-        playerSceneScoreText.text = playerScore.ToString();
+        UpdateCharacterScore(playerScore);
+        //playerSceneScoreText.text = playerScore.ToString();
+
+        
     }
 
     private void UpdateScoreEnemy(int Score)
@@ -49,6 +66,16 @@ public class GameUI : MonoBehaviour {
         //print("updating score");
         enemyScore += Score;
         EnemyScoreText.text = "ENEMY SCORE: " + enemyScore.ToString();
-        enemySceneScoreText.text = enemyScore.ToString();
+        // enemySceneScoreText.text = enemyScore.ToString();
     }
+
+    public int getPlayerScore()
+    {
+        return playerScore;
+    }
+    public int getEnemyScore()
+    {
+        return enemyScore;
+    }
+
 }
