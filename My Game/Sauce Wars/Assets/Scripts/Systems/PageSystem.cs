fb7ea55637ddd5461d29c556dae02a1a73f1a1d4
ninjerie <38001901+ninjerie@users.noreply.github.com>
2019-01-16 04:21:00 +0000

    Updates
    -Made HB images
    -Added HB images to game
    -Added HB to instructions
    -Adjusted some buttons and text to be uniform

--- My Game/Sauce Wars/Assets/Scripts/Systems/PageSystem.cs
+++ My Game/Sauce Wars/Assets/Scripts/Systems/PageSystem.cs
@@ -13,12 +13,16 @@ public class PageSystem : MonoBehaviour {
     public GameObject page2;
     public GameObject page3;
     public GameObject page4;
+    public GameObject page5;
+    public GameObject page6;
 
     // Use this for initialization
     void Start () {
         page2.SetActive(false);
         page3.SetActive(false);
         page4.SetActive(false);
+        page5.SetActive(false);
+        page6.SetActive(false);
         backButton.SetActive(false);
 	}
 	
@@ -50,6 +54,18 @@ public class PageSystem : MonoBehaviour {
             page4.SetActive(true);
             page3.SetActive(false);
             currentPage = "Page 4";
+        }
+        else if (currentPage == "Page 4")
+        {
+            page5.SetActive(true);
+            page4.SetActive(false);
+            currentPage = "Page 5";
+        }
+        else if (currentPage == "Page 5")
+        {
+            page6.SetActive(true);
+            page5.SetActive(false);
+            currentPage = "Page 6";
             nextButton.SetActive(false);
         }
     }
@@ -74,6 +90,18 @@ public class PageSystem : MonoBehaviour {
             page3.SetActive(true);
             page4.SetActive(false);
             currentPage = "Page 3";
+        }
+        else if (currentPage == "Page 5")
+        {
+            page4.SetActive(true);
+            page5.SetActive(false);
+            currentPage = "Page 4";
+        }
+        else if (currentPage == "Page 6")
+        {
+            page5.SetActive(true);
+            page6.SetActive(false);
+            currentPage = "Page 5";
             nextButton.SetActive(true);
         }
 
